b38a6f5eeaa5183f255e912abf5b8409668700e7
Baheer Noori <96612910+NarutBaheer@users.noreply.github.com>
2024-03-09 18:41:40 +0430

    Add files via upload

---
+++ video-trailer-popup/index.js
@@ -0,0 +1,14 @@
+const btnE1 = document.querySelector(".btn");
+const closeIconE1 = document.querySelector(".close-icon");
+const trailerContainerE1 = document.querySelector(".trailer-container");
+
+btnE1.addEventListener("click", () => {
+    trailerContainerE1.classList.remove("active");
+});
+
+closeIconE1.addEventListener("click", () => {
+    trailerContainerE1.classList.add("active");
+    videoE1.pause();
+    videoE1.currentTime = 0;
+    
+});
